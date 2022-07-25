74b920a777c350f4d4ab1d03f994421c2785839b
Richard Iannone <riannone@me.com>
2022-07-25 13:43:13 -0400

    Create css_inline.R

---
+++ R/css_inline.R
@@ -0,0 +1,21 @@
+#' Perform CSS inlining with HTML
+#' 
+#' Given an HTML string, the `css_inline()` function will render that into HTML
+#' with CSS inlined into the `style` attribute of HTML tags.
+#' 
+#' @param html HTML provided as a string.
+#' 
+#' @return A character vector of length one which contains HTML text.
+#' 
+#' @export 
+css_inline <- function(html) {
+  
+  js_file <- system.file("dist/bundle.js", package = "juicyjuice")
+  
+  ct <- V8::v8()
+  ct$source(js_file)
+  ct$assign("html_text", html)
+  ct$assign("options", NULL)
+  ct$eval("var inlined = juice(html_text, options);")
+  ct$get("inlined")
+}
